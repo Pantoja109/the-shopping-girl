7cb090451f217b8df9dbaf33e05f5ef76aa4ed93
Pantoja109 <andres08_93@hotmail.com>
2022-11-01 17:24:34 -0600

    initial config

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbyzIPK1lsVJF1esmo4fjGxl2kfHJ3Kvc",
+  authDomain: "the-shopping-girl.firebaseapp.com",
+  projectId: "the-shopping-girl",
+  storageBucket: "the-shopping-girl.appspot.com",
+  messagingSenderId: "583045622574",
+  appId: "1:583045622574:web:81e1dae0967a3cd478e8bf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
